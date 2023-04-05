c9f0eb3552470dd58baef69d7af86b836c639d31
eunsu28 <ayay.e2008@gmail.com>
2023-04-05 21:27:49 +0900

    make every cards to nimber

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 clear = lambda : os.system('clear')
 clear()
 
-card = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
+card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
 #diller = random.choice(card)
 mine = random.sample(card, 2)
 print(mine)
