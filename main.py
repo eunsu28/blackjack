0e8c75eb36064a53a0f0247e0ca43a31072c45e8
eunsu28 <ayay.e2008@gmail.com>
2023-05-27 14:21:21 +0900

    I'm back

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ clear()
 card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
 #diller = random.sample(card, 2)
 mine = random.sample(card, 2)
+second_card = random.sample(card, 1)
+
 print(mine)
 
 mine_result = sum(mine)
 print(mine_result)
+
+print(second_card)
