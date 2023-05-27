167659efb2934e51a73088fa817bca2927f0f116
eunsu28 <ayay.e2008@gmail.com>
2023-05-27 14:30:12 +0900

    success2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import os
+from time import sleep as s
 
 clear = lambda : os.system('clear')
 clear()
@@ -8,16 +9,20 @@ card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
 #diller = random.sample(card, 2)
 mine = random.sample(card, 2)
 second_card = random.choice(card)
-
-print(mine)
-
 mine_result = sum(mine)
-print(mine_result)
 
-print(second_card)
+
+print("your cards", mine)
+print("your sub-card", second_card)
 
 choice = input("add or not:")
 
 if choice == "add":
     final_result = mine_result + second_card
     print(final_result)
+    s(1)
+    if final_result > 21:
+        print("you failed")
+        clear()
+else:
+    print()
