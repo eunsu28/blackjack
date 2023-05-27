299982489a012cfe89a54541865a1099f64b251b
eunsu28 <ayay.e2008@gmail.com>
2023-05-27 14:32:41 +0900

    test2

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ clear = lambda : os.system('clear')
 clear()
 
 card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
-#diller = random.sample(card, 2)
+diller = random.sample(card, 2)
 mine = random.sample(card, 2)
 second_card = random.choice(card)
 mine_result = sum(mine)
 
 
-print("your cards", mine)
-print("your sub-card", second_card)
+print("your cards: ", mine)
+print("your sub-card: ", second_card)
 
-choice = input("add or not:")
+choice = input("add or not: ")
 
 if choice == "add":
     final_result = mine_result + second_card
