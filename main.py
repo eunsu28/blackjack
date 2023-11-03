33a4aaaa66959d6f535329eac71aa27605a75c05
eunsu28 <ayay.e2008@gmail.com>
2023-11-03 11:45:55 +0900

    while

--- main.py
+++ main.py
@@ -2,41 +2,52 @@ import random
 import os
 from time import sleep as s
 
-clear = lambda : os.system('clear')
-clear()
+on_game = True
 
-card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
+while on_game:
+    clear = lambda : os.system('clear')
+    clear()
 
-diller = sum(random.sample(card, 2))
-mine = random.sample(card, 2)
+    card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11]
 
-second_card = random.choice(card)
-mine_result = sum(mine)
-final_result = mine_result + second_card
+    diller = sum(random.sample(card, 2))
+    mine = random.sample(card, 2)
+
+    second_card = random.choice(card)
+    mine_result = sum(mine)
+    final_result = mine_result + second_card
 
 
-print("your cards: ", mine)
-print("your sub-card: ", second_card)
+    print("your cards: ", mine)
+    print("your sub-card: ", second_card)
 
-choice = input("add or not: ")
+    choice = input("add or not: ")
 
-if choice == "add":
-    print(final_result)
-    mine_result = final_result
-    s(1)
-else:
-    print(mine_result)
+    if choice == "add":
+        print(final_result)
+        mine_result = final_result
+        s(1)
+    elif choice == "off":
+        on_game = False
+    else:
+        print(mine_result)
 
-if mine_result > 21:
-    print("you failed")
-    s(1)
-    print("diller's number was: ", diller)
-else:
-    print("diller's number: ", diller)
+    if mine_result > 21:
+        print("you failed")
+        s(1)
+        if diller == 21:
+            print("blackjack")
+            print("diller's number was: ", diller)
+        else:
+            print("diller's number was: ", diller)
+    else:
+        print("diller's number: ", diller)
 
-if mine_result > diller:
-    print("you win")
-elif mine_result == diller:
-    print("tie")
-else:
-    print("you lose")
+    if mine_result > diller:
+        print("you win")
+    elif mine_result == diller:
+        print("tie")
+    else:
+        print("you lose")
+
+    s(3)
